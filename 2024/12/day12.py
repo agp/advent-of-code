821c259a961b2ceb9f49a3297370d5b79f46f042
agp <agpacker@gmail.com>
2024-12-12 18:45:11 -0500

    day 12 part 2

--- 2024/12/day12.py
+++ 2024/12/day12.py
@@ -22,7 +22,6 @@ def validity(
         not_visited = not visited[row][col]
         is_same_plant = plant == matrix[row][col]
     return in_bounds, not_visited, is_same_plant
-    
 
 
 def bfs_plants(row: int, col: int, matrix: list[list[str]], visited, start):
@@ -30,6 +29,7 @@ def bfs_plants(row: int, col: int, matrix: list[list[str]], visited, start):
 
     q.append((row, col))
     visited[row][col] = True
+    plots[start]["points"] = set()
 
     while len(q) > 0:
         cell = q.popleft()
@@ -37,20 +37,20 @@ def bfs_plants(row: int, col: int, matrix: list[list[str]], visited, start):
         cell_col = cell[1]
         plant = matrix[cell_row][cell_col]
         plots[start]["count"] += 1
+        plots[start]["points"].add((cell_col, cell_row))
 
         for i in range(4):
             next_row = cell_row + dr[i]
             next_col = cell_col + dc[i]
-            in_bounds, not_visited, is_same_plant = validity(next_row, next_col, matrix, visited, plant)
+            in_bounds, not_visited, is_same_plant = validity(
+                next_row, next_col, matrix, visited, plant
+            )
             # cell is in bounds, not visited and same plant - add to queue
-            
             if all((in_bounds, not_visited, is_same_plant)):
                 q.append((next_row, next_col))
                 visited[next_row][next_col] = True
             # cell is out of bounds or in bounds with different plant:
-            elif not in_bounds or (
-                in_bounds and not is_same_plant
-            ):
+            elif not in_bounds or (in_bounds and not is_same_plant):
                 plots[start]["fences"] += 1
 
 
@@ -59,9 +59,64 @@ plots = defaultdict(lambda: defaultdict(int))
 
 for i, row in enumerate(input):
     for j, col in enumerate(row):
-        in_bounds, not_visited, is_same_plant = validity(i, j, input, visited, input[i][j])
+        in_bounds, not_visited, is_same_plant = validity(
+            i, j, input, visited, input[i][j]
+        )
         if not_visited:
             start = f"x{j},y{i}"
             bfs_plants(i, j, input, visited, start)
 
 print(sum([p["count"] * p["fences"] for p in plots.values()]))
+
+# part 2
+# copied from solutions... was wrinkling my brain.
+def count_corners(area):
+    up = set()
+    down = set()
+    left = set()
+    right = set()
+    for (x, y) in area:
+        if (x - 1, y) not in area:
+            left.add((x, y))
+
+        if (x + 1, y) not in area:
+            right.add((x, y))
+
+        if (x, y + 1) not in area:
+            down.add((x, y))
+
+        if (x, y - 1) not in area:
+            up.add((x, y))
+
+    corners = 0
+    for (x, y) in left:
+        if (x, y) in up:
+            corners += 1
+        if (x, y) in down:
+            corners += 1
+        if (x - 1, y - 1) in down and (x, y) not in up:
+            corners += 1
+        if (x - 1, y + 1) in up and (x, y) not in down:
+            corners += 1
+
+    for (x, y) in right:
+        if (x, y) in up:
+            corners += 1
+        if (x, y) in down:
+            corners += 1
+        if (x + 1, y - 1) in down and (x, y) not in up:
+            corners += 1
+        if (x + 1, y + 1) in up and (x, y) not in down:
+            corners += 1
+
+    return corners
+
+# corners = count_corners(plots_sorted)
+# print(corners)
+
+part2 = 0
+for k, v in plots.items():
+    area_sorted = sorted(v["points"], key=lambda x: tuple(reversed(x)))
+    part2 += v["count"] * count_corners(area_sorted)
+    
+print(part2)
