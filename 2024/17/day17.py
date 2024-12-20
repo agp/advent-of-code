f7e885f7b927651597af9ed31444724e03a5e903
agp <agpacker@gmail.com>
2024-12-19 21:15:54 -0500

    day 17 part 2 WIP

--- 2024/17/day17.py
+++ 2024/17/day17.py
@@ -1,30 +1,56 @@
 import re
+import time
 
 with open("./17/input.txt") as f:
     input = f.read().strip()
 
 
 class Computer:
-    output: list = []
-    pointer: int = 0
-    A: int
-    B: int
-    C: int
 
-    def __init__(self, input):
+    def __init__(self, input: str, init_A: int = None) -> None:
+        self.A: int = 0
+        self.B: int = 0
+        self.C: int = 0
+        self.pointer: int = 0
+        self.output: list = []
+        self.input = input
         registers, program = input.split("\n\n")
         for r in registers.split("\n"):
             r_re = re.search(r"Register ([ABC]): (\d+)", r)
             setattr(self, r_re.group(1), int(r_re.group(2)))
         self.program = [int(c) for c in program.split(": ")[1].split(",")]
+        if init_A:
+            self.init_A = init_A
+            self.A = init_A
+        self.init_B = self.B
+        self.init_C = self.C
+
+    # reset computer with new initial A register
+    def reset(self, init_A: int) -> None:
+        self.A = self.init_A = init_A
+        self.B = self.init_B
+        self.C = self.init_C
+        self.output = []
+        self.pointer = 0
 
     # run program until opcode is past end of program
-    def run(self):
+    def run(self) -> None:
         while self.pointer < len(self.program):
+            print(self.output)
             self.execute()
 
+    # run but exit early if out and program don't align
+    def run_to_copy(self) -> None:
+        while self.pointer < len(self.program):
+            if self.output == self.program[0 : len(self.output)] and len(
+                self.output
+            ) <= len(self.program):
+                self.execute()
+            else:
+                break
+
     # execute program block of opcode and operand
-    def execute(self):
+    def execute(self) -> None:
         opcode = self.program[self.pointer]
         operand = self.program[self.pointer + 1]
         adv_pointer = True
@@ -57,7 +83,7 @@ class Computer:
         self.pointer += 2 if adv_pointer else 0
 
     # return combo case for operand
-    def get_combo(self, operand):
+    def get_combo(self, operand) -> int:
         match operand:
             case 0 | 1 | 2 | 3:
                 return operand
@@ -73,9 +99,32 @@ class Computer:
             case _:
                 raise ValueError
 
-    def print(self):
+    def print(self) -> None:
         print(",".join([str(i) for i in self.output]))
-        
+
+
 computer = Computer(input)
 computer.run()
 computer.print()
+
+# part 2 - brute force does not work...
+
+# init_A = 117440
+# init_A = 8
+# c2 = Computer(input, init_A)
+
+# c2 = Computer(input, 2)
+# output = None
+# program = c2.program
+# start_time = time.time()
+# while output != program:
+#     # init_A += 1
+#     init_A = init_A ** 8
+#     c2.reset(init_A)
+#     # c2.run_to_copy()
+#     c2.run()
+#     output = c2.output
+
+# print(f"copy at A = {init_A}")
+# end_time = time.time()
+# print(f"time to run: {end_time-start_time}s")
