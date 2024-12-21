55230116adeffb4f3caec331fc3727130a4d21b0
agp <agpacker@gmail.com>
2024-12-21 13:20:49 -0500

    day 15 part 2, works for test cases but not input

--- 2024/15/day15.py
+++ 2024/15/day15.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import deque as deque
 
-with open("./15/input.txt") as f:
+with open("./15/test9.txt") as f:
     input = f.read().strip()
 
 split = input.split("\n\n")
@@ -11,38 +11,55 @@ input_moves = [m for m in split[1] if m != "\n"]
 
 
 class Warehouse:
-    def __init__(self, raw_grid: list[str]):
+    def __init__(self, raw_grid: list[str], wide: bool = False) -> None:
+        if wide:
+            raw_grid = [
+                "".join([self.go_wide(cell) for cell in line]) for line in raw_grid
+            ]
         self.grid = {
             (x, y): val for y, line in enumerate(raw_grid) for x, val in enumerate(line)
         }
-        self.rows, self.col = (
+
+        self.cols, self.rows = (
             val + 1 for val in np.max(list(self.grid.keys()), axis=0)
         )
 
-    def gps(self):
-        sums_list = [
-            pos[1] * 100 + pos[0]
-            for pos, val in robot.warehouse.grid.items()
-            if val == "O"
-        ]
-        return np.sum(np.array(sums_list))
+    @staticmethod
+    def go_wide(cell: str) -> str:
+        match cell:
+            case "#":
+                return "##"
+            case "O":
+                return "[]"
+            case ".":
+                return ".."
+            case "@":
+                return "@."
 
-    def print(self):
-        split = np.array_split(list(self.grid.values()), self.rows)
-        for row in split:
-            print("".join([self.get_color(cell) for cell in row]))
-        pass
-
-    def get_color(self, cell: str):
+    @staticmethod
+    def get_color(cell: str) -> str:
         match cell:
             case "@":
                 return f"\033[31;1m{cell}\033[0m"
             case "#":
                 return f"\033[36;47m{cell}\033[0m"
-            case "O":
+            case "O" | "[" | "]":
                 return f"\033[32;1m{cell}\033[0m"
             case ".":
                 return f"\033[90;m{cell}\033[0m"
+
+    def print(self) -> None:
+        split = np.array_split(list(self.grid.values()), self.rows)
+        for row in split:
+            print("".join([self.get_color(cell) for cell in row]))
+        pass
+
+    def gps(self):
+        sums_list = [
+            pos[1] * 100 + pos[0] for pos, val in self.grid.items() if val == "O" or val == "["
+        ]
+        return np.sum(np.array(sums_list))
+
 
 
 class Robot:
@@ -52,25 +69,32 @@ class Robot:
         "v": np.array([0, 1]),
         "<": np.array([-1, 0]),
     }
+    box_sides = {"]": np.array([-1, 0]), "[": np.array([1, 0])}
 
     def __init__(self, warehouse: Warehouse, moves: list[str]) -> None:
 
         self.warehouse = warehouse
         self.grid = warehouse.grid
-        self.pos = self.find(self.grid)
+        # self.pos = self.find(self.grid)
         self.move_q = deque(moves)
 
-    def find(self, grid) -> tuple[int, int]:
-        return tuple(list(grid.keys())[list(grid.values()).index("@")])
+    @property
+    def pos(self):
+        return tuple(list(self.grid.keys())[list(self.grid.values()).index("@")])
 
     def run(self) -> None:
-        # i = 0
+        i = 0
         while len(self.move_q) > 0:
-            # i += 1
+            i += 1
             m = self.move_q.popleft()
-            self.move(m)
+            x, y = self.dirs[m]
+            if y != 0:
+                self.move_wide(m)
+            else:
+                self.move(m)
             # print(f"move {i}: {m}")
             # self.warehouse.print()
+            # print("continuing")
 
     def move(self, move: str) -> None:
         q = self.get_next_cells(self.dirs[move])
@@ -85,11 +109,60 @@ class Robot:
                 # there is free space. swap the last with the previous
                 self.warehouse.grid[prev_pos] = last_val
                 self.warehouse.grid[last_pos] = prev_val
-                # set robot pos if we are at it
-                if prev_val == "@":
-                    self.pos = last_pos
             # append the last with the previous pos
             q.append((prev_pos, last_val))
+
+    def move_wide(self, move: str) -> None:
+        move_dir = self.dirs[move]
+        next_cell_pos = tuple((self.pos + move_dir).tolist())
+        next_cell = self.grid[next_cell_pos]
+        if next_cell in self.box_sides.keys():
+            cells = [self.pos]
+            robot_neighbor = tuple((self.pos + self.box_sides[next_cell]).tolist())
+            # don't shift the robot neighbor if it is a box
+            if self.grid[robot_neighbor] not in self.box_sides.keys():
+                cells.append(robot_neighbor)
+            chain = self.get_box_chain([cells], move_dir)
+            # shift the chain in reverse
+            for row in chain[::-1]:
+                for pos in row:
+                    new_pos = tuple((pos + move_dir).tolist())
+
+                    self.grid[new_pos] = self.grid[pos]
+                    self.grid[pos] = "."
+                    # self.warehouse.print()
+        else:
+            self.move(move)
+
+    def get_box_chain(self, cells: list[tuple[int, int]], move_dir) -> list:
+        next_cells = {}
+        last_cells = cells[-1]
+        for c in last_cells:
+            # print(c)
+            next_cell_pos = tuple((np.array(c) + move_dir).tolist())
+            next_cell_val = self.grid[next_cell_pos]
+            # print(next_cell_pos, next_cell_val)
+            next_cells[next_cell_pos] = next_cell_val
+            # if the cell is a box, check it for box side neighbors and add to next_cells
+            if next_cell_val in self.box_sides.keys():
+                box_neighbor = tuple(
+                    (np.array(next_cell_pos) + self.box_sides[next_cell_val]).tolist()
+                )
+                if self.grid[box_neighbor] in self.box_sides.keys():
+                    next_cells[box_neighbor] = self.grid[box_neighbor]
+        # print(next_cells)
+        # if all next_cells have free space we can return the chain and move
+        if all([val == "." for val in next_cells.values()]):
+            # cells.extend([list(next_cells.keys())])
+            return cells
+        # there is a wall in the box chain. do nothing
+        elif any([val == "#" for val in next_cells.values()]):
+            return []
+        # there are boxes, continue the chain
+        elif any([val in self.box_sides.keys() for val in next_cells.values()]):
+            only_box_cells = [pos for pos, val in next_cells.items() if val in self.box_sides.keys()]
+            cells.extend([only_box_cells])
+            return self.get_box_chain(cells, move_dir)
 
     def get_next_cells(self, vector: tuple[int, int]) -> deque:
         q = deque()
@@ -106,8 +179,19 @@ class Robot:
         return q
 
 
+# part 1
+
 grid = Warehouse(input_grid)
 robot = Robot(grid, input_moves)
 robot.run()
 robot.warehouse.print()
 print(robot.warehouse.gps())
+
+# part 2
+grid2 = Warehouse(input_grid, True)
+robot2 = Robot(grid2, input_moves)
+print("initial state")
+robot2.warehouse.print()
+robot2.run()
+robot2.warehouse.print()
+print(robot2.warehouse.gps())
